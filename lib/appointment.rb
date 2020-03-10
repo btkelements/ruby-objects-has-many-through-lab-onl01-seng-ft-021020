43f35d1838df94caff12ddbf2777d76eb61472fb
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-03-10 19:38:10 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,8 +1,15 @@
 class Appointment
   @@all = []
   
-  def initialize ("", patient, doctor)
-    @"" = ""
-    @patient = 
+  def initialize (date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
   
 end
