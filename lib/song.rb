50e0e95b87a83e173877859fbca1c5962c01d36d
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-03-10 19:16:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
   @@all = []
   attr_accessor :name, :artist, :genre
-  def initialize (name, artist, genre)
+  def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
