f40ba2441f74e883bb883fddaa8e69b6cef60687
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-03-10 19:32:39 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,13 +1,21 @@
 class Doctor
   attr_accessor :name
   @@all = []
-  def initialize(name)
+  def initialize (name)
     @name = name
     @@all << self
   end
   
   def self.all
     @@all
+  end
+  
+  def appointments
+    Appointment.all.select{|appointment| appointment.doctor == self}
+  end
+  
+  def patients
+    appointments.collect {|appointment| appointments.patient}
   end
   
 end
