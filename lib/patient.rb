2d08415aa07c84089bdd3bf0e1d4333cb6770db6
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-03-10 19:45:33 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,4 +1,17 @@
 class Patient
-  attr_accessor :name, :appointment, :doctor
+  @@all = []
+  
+  def initialize (name)
+    @name = name
+    @@all << self
+  end
+  
+  def  self.all
+    @@all
+  end
+  
+  def new_appointment(doctor, date)
+    appointment = Appointment.new(date, self, doctor)
+  end
   
 end
